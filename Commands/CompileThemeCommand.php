7c6313fbe438240ecfc7519ae48a263810589096
Uwe Kleinmann <uwe@kleinmann.org>
2017-07-11 18:03:05 +0200

    [noticket] Fix theme compilation by using the actual theme compile method.

--- Commands/CompileThemeCommand.php
+++ Commands/CompileThemeCommand.php
@@ -1,15 +1,11 @@
 <?php
 namespace Shopware\Plugin\K10rDeployment\Command;
 
-use Shopware\Bundle\PluginInstallerBundle\Service\InstallerService;
 use Shopware\Commands\ShopwareCommand;
 use Shopware\Components\Theme\Compiler;
 use Shopware\Models\Shop\Repository;
 use Shopware\Models\Shop\Shop;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class K10rCompileThemeCommand extends ShopwareCommand
@@ -38,11 +34,9 @@ class K10rCompileThemeCommand extends ShopwareCommand
 
         foreach ($repository->getActiveShops() as $shop) {
             /** @var Shop $shop */
-            $compiler->compileJavascript('new', $shop->getTemplate(), $shop);
-            $compiler->compileLess('new', $shop->getTemplate(), $shop);
+            $compiler->compile($shop);
 
             $output->writeln(sprintf('Theme for shop "%s (#%d)" has been compiled.', $shop->getName(), $shop->getId()));
         }
-
     }
 }
